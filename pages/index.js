335f2403b610363fa4acf860305b382e966bc672
nam-mihail <nam.mihail.97@gmail.com>
2025-08-31 00:24:22 +0900

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,10 @@ import WeddingInviteBook from "../components/WeddingInviteBook";
   <link rel="preconnect" href="https://fonts.googleapis.com"></link>
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
   <link
-    href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap"
+    href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
     rel="stylesheet"
   ></link>
 </div>;
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return <WeddingInviteBook />;
