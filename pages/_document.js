9b785599d45834cbaed4be8721b4d55742ee397e
nam-mihail <nam.mihail.97@gmail.com>
2025-08-31 09:37:28 +0900

    update

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="color-scheme" content="light" />
+        <meta name="color-scheme" content="only light" />
         <meta name="supported-color-schemes" content="light" />
+        <meta name="theme-color" content="#ffffff"></meta>
       </Head>
       <body className="antialiased">
         <Main />
