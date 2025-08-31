634be8695ca4ba14957fa91b1d10bbffee0a04bb
nam-mihail <nam.mihail.97@gmail.com>
2025-08-31 09:27:00 +0900

    update

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import WeddingInviteBook from "../components/WeddingInviteBook";
     href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
     rel="stylesheet"
   ></link>
+  <meta name="color-scheme" content="light"></meta>
 </div>;
 
 export default function Home() {
